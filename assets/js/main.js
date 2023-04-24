6fcdaf111eca6630a38cd5a3511eb38b24da17f5
englucianasena <englucianasena@outlook.com>
2023-04-23 22:20:57 -0300

    Correção resposividade

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,27 +1,26 @@
-// Mostrar Menu
-
-const showMenu = (toggleId, navId) => {
+/* === Mostrar Menu === */
+const showMenu = (toggleId, navId) =>{
     const toggle = document.getElementById(toggleId),
         nav = document.getElementById(navId)
 
-    if (toggle && nav) {
-        toggle.addEventListener('click', () => {
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
             nav.classList.toggle('show-menu')
         })
     }
 }
 showMenu('nav-toggle', 'nav-menu')
 
-// Remove Menu Mobile
+/* === Remove Menu Mobile === */
 const navLink = document.querySelectorAll('.nav__link')
 
-function linkAction() {
+function linkAction(){
     const navMenu = document.getElementById('nav-menu')
     navMenu.classList.remove('show-menu')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
-/* Link ativo das seções de rolagem */
+/* Ativar link das seções de rolagem */
 const sections = document.querySelectorAll('section[id]') /*Erro na querySelector(selectors: string) -> sectionId*/ 
 
 function scrollActive(){
@@ -32,7 +31,7 @@ function scrollActive(){
         const sectionTop = current.offsetTop - 50
         sectionId = current.getAttribute('id')
 
-        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
             document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
         } else {
             document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
@@ -42,24 +41,24 @@ function scrollActive(){
 window.addEventListener('scroll',scrollActive)
 
 
-/* Mudar o Background do Header */
+/* === Mudar o Background do Header === */
 function scrollHeader(){
     const header = document.getElementById('header')
-    if(this.scrollY >= 200) header.classList.add('scroll-header'); 
+    if(this.scrollY >= 200) header.classList.add('scroll-header') 
     else header.classList.remove('scroll-header')
 }
 window.addEventListener('scroll', scrollHeader)
 
-/* Mostrar rolagem do topo */
+/* === Mostrar rolagem do topo === */
 function scrollTop(){
     const scrollTop = document.getElementById('scroll-top')
-    if(this.scrollY >= 560) scrollTop.classList.add('show-scroll'); 
+    if(this.scrollY >= 560) scrollTop.classList.add('show-scroll') 
     else scrollTop.classList.remove('show-scroll')
 }
 window.addEventListener('scroll', scrollTop)
 
 
-/* Mixitup filtro do Portfolio */
+/* === Mixitup filtro do Portfolio === */
 const mixer = mixitup('.portfolio__container', {
     selectors: {
         target: '.portfolio__content'
@@ -78,6 +77,32 @@ function activePortfolio(){
         this.classList.add('active-portfolio')
     }
 }
-linkPortfolio.forEach(l => l.addEventListener('click', activePortfolio)) 
-// Corrigir!
+linkPortfolio.forEach(l => l.addEventListener('click', activePortfolio))
 
+/* === Swiper Carousel === */
+const mySwiper = new Swiper('.testimonial__container', {
+    spaceBetween: 16,
+    loop: true,
+    grabCursor: true,
+  
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    breakpoints:{
+        640:{
+            slidesPerview: 2,
+        },
+        1024:{
+            slidesPerview: 3,
+        },  
+    }
+  });
+
+/* === Gsap Animation === */
+gsap.from('.home__img', {opacity: 0, duration: 2, delay: .5, x:60})
+gsap.from('.home__data', {opacity: 0, duration: 2, delay: .8, y:25})
+gsap.from('.home__greeting, .home__name, .home__profession, .home__button', {opacity: 0, duration: 2, delay:1, y:25, ease:'expo.out', stagger: .2})
+gsap.from('.nav__logo, .nav__toggle', {opacity: 0, duration: 2, delay:1.5, y:25, ease:'expo.out', stagger: .2})
+gsap.from('.nav__item', {opacity: 0, duration: 2, delay:1.8, y:25, ease:'expo.out', stagger: .2})
+gsap.from('.home__social-icon', {opacity: 0, duration: 2, delay:2.3, y:25, ease:'expo.out', stagger: .2})
