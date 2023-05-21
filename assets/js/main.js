3ecada792c4d86669507765b6ef53297402d1577
englucianasena <englucianasena@outlook.com>
2023-05-21 19:10:03 -0300

    Ajustes Nav

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,60 +3,62 @@ const showMenu = (toggleId, navId) =>{
     const toggle = document.getElementById(toggleId),
         nav = document.getElementById(navId)
 
+    // Valide se as variáveis existem
     if(toggle && nav){
         toggle.addEventListener('click', ()=>{
+            // Adicionar a class show-menu à tag div com a class nav__menu
             nav.classList.toggle('show-menu')
         })
     }
 }
 showMenu('nav-toggle', 'nav-menu')
 
-/* === Remove Menu Mobile === */
+/* === REMOVER MENU MOBILE === */
 const navLink = document.querySelectorAll('.nav__link')
 
 function linkAction(){
     const navMenu = document.getElementById('nav-menu')
+    // Quando clicamos em cada nav__link, removemos a class show-menu
     navMenu.classList.remove('show-menu')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
-/* Ativar link das seções de rolagem */
-const sections = document.querySelectorAll('section[id]') /*Erro na querySelector(selectors: string) -> sectionId*/ 
+/* === SCROLL SECTIONS ACTIVE LINK === */
+const sections = document.querySelectorAll('section[id]')
 
-function scrollActive(){
+function scrollActive() {
     const scrollY = window.pageYOffset
 
-    sections.forEach(current =>{
+    sections.forEach(current => {
         const sectionHeight = current.offsetHeight
-        const sectionTop = current.offsetTop - 50
+        const sectionTop = current.offsetTop - 50;
         sectionId = current.getAttribute('id')
 
-        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
             document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
         } else {
             document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
         }
     })
 }
-window.addEventListener('scroll',scrollActive)
+window.addEventListener('scroll', scrollActive)
 
 
-/* === Mudar o Background do Header === */
-function scrollHeader(){
-    const header = document.getElementById('header')
-    if(this.scrollY >= 200) header.classList.add('scroll-header') 
-    else header.classList.remove('scroll-header')
+/* === MUDAR BACKGROUND HEADER === */
+function scrollHeader() {
+    const nav = document.getElementById('header')
+    // Quando a rolagem for maior que 200 de altura da janela de visualização, adicione a classe scroll-header à tag do cabeçalho
+    if (this.scrollY >= 200) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
 }
 window.addEventListener('scroll', scrollHeader)
 
-/* === Mostrar rolagem do topo === */
-function scrollTop(){
-    const scrollTop = document.getElementById('scroll-top')
-    if(this.scrollY >= 560) scrollTop.classList.add('show-scroll') 
-    else scrollTop.classList.remove('show-scroll')
+/* === MOSTRAR SCROLL TOP === */
+function scrollTop() {
+    const scrollTop = document.getElementById('scroll-top');
+    // Quando a rolagem for maior que 560 de altura da janela de visualização, adicione a classe show-scroll à tag a com a classe scroll-top
+    if (this.scrollY >= 560) scrollTop.classList.add('scroll-top'); else scrollTop.classList.remove('scroll-top')
 }
 window.addEventListener('scroll', scrollTop)
-
 
 /* === Mixitup filtro do Portfolio === */
 const mixer = mixitup('.portfolio__container', {
